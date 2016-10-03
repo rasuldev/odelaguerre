5da64a3be8f3d8088f43eb85882e7a6df52b300b
Rasul <rasuldev@gmail.com>
2016-10-03 22:33:31 +0300

    some simple changes

---
+++ OdeWithLaguerre/OdeSolver.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using mathlib;
+using mathlib.Polynomials;
+using static System.Math;
+
+namespace OdeWithLaguerre
+{
+    public class OdeSolver
+    {
+        private Func<double, double> _right;
+        private double[] _coeffs;
+        private double[] _initialConditions;
+
+        /// <summary>
+        /// y^{(r)}(x) + a_{r-1} y^{(r-1)}(x) +... a_0 y(x) = h(x)
+        /// </summary>
+        /// <param name="right">h(x)</param>
+        /// <param name="initialConditions"></param>
+        /// <param name="coeffs">coeffs[i] corresponds to a_i</param>
+        public OdeSolver(Func<double, double> right, double[] initialConditions, double[] coeffs)
+        {
+            _right = right;
+            if (initialConditions.Length != coeffs.Length)
+                throw new ArgumentException("initialConditions must have the same length as coeffs");
+            _initialConditions = initialConditions;
+            _coeffs = coeffs;
+
+        }
+
+        public double QCoeffs(int nu)
+        {
+            int r = _coeffs.Length;
+            var sum = 0d;
+            for (int j = 0; j < r; j++)
+            {
+                var sumj = 0d;
+                for (int k = 0; k <= nu; k++)
+                {
+                    sumj += Pow(-1, k) * Binomial.Calc(nu, k) * Binomial.Calc(r + k - j - 1, k);
+                }
+                sum += _coeffs[j] * sumj;
+            }
+            return sum;
+        }
+
+        public double PCoeffs(int s)
+        {
+            return HCoeffs(s) - PSecondPart(s);
+            
+        }
+
+        /// <summary>
+        /// $\sum_{\nu=0}^{r-1} a_\nu \sum_{k=0}^{r-\nu-1} y^{(k+\nu)}(0) \sum_{j=0}^s (-1)^j \binom{s}{j} \binom{k+j}{j}
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        protected double PSecondPart(int s)
+        {
+            var sum = 0d;
+            var r = _coeffs.Length;
+            for (int nu = 0; nu <= r - 1; nu++)
+            {
+                var innerSum = 0d;
+                for (int k = 0; k <= r - nu - 1; k++)
+                {
+                    innerSum += _initialConditions[k + nu] * BinomsProductSum(s, k);
+                }
+                sum += _coeffs[nu] * innerSum;
+            }
+            return sum;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="k"></param>
+        /// <returns>Sum $\sum_{j=0}^s (-1)^j \binom{s}{j} \binom{k+j}{j} $</returns>
+        protected static int BinomsProductSum(int s, int k)
+        {
+            var sum = 1;
+            var sign = -1;
+            for (int j = 1; j <= s; j++)
+            {
+                sum += sign * Binomial.Calc(s, j) * Binomial.Calc(k + j, j);
+            }
+            return sum;
+        }
+
+        public double HCoeffs(int s)
+        {
+            var coeffSym = mathlib.ScalarMul.LebesgueLaguerre(_right, Laguerre.Get(s));
+            return Integrals.RectangularInfinite(coeffSym, 100000, 10);
+        }
+
+    }
+}
---
+++ Tests/OdeSolverTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using OdeWithLaguerre;
+using static System.Math;
+using static System.Linq.Enumerable;
+
+namespace Tests
+{
+    [TestFixture]
+    public class OdeSolverTests
+    {
+        [Test]
+        public void QTest()
+        {
+            var solver = new OdeSolver(x => x, new[] { 1d, 2, 3 }, new[] { 1d, 2, 3 });
+            // Q[0] should be equal to sum of coeffs a_i, i=0, 1,..., r-1
+            Assert.AreEqual(1 + 2 + 3, solver.QCoeffs(0));
+        }
+
+        [Test]
+        public void Q2Test()
+        {
+            var a = new[] { 1d, 2, 3 };
+            Func<double, double> q = x =>
+            {
+                var sum = 0d;
+                var r = a.Length;
+                for (int k = 0; k < a.Length; k++)
+                {
+                    sum += a[k] * Pow(x, r - k - 1) / fact(r - k - 1);
+                }
+                return sum;
+            };
+            var solver = new OdeSolver(x => x, a, a);
+
+            var qCoeffs = Range(0, 3).Select(solver.QCoeffs).ToArray();
+            var Laguerre = Range(0, 3).Select(mathlib.Polynomials.Laguerre.Get).ToArray();
+            Func<double, double> qLaguerreExpansion = x =>
+            {
+                var sum = 0d;
+                for (int k = 0; k < qCoeffs.Length; k++)
+                {
+                    sum += qCoeffs[k] * Laguerre[k](x);
+                }
+                return sum;
+            };
+
+            var t = 0d;
+            while (t < 100)
+            {
+                Assert.That(q(t), Is.EqualTo(qLaguerreExpansion(t)).Within(0.0001));
+                t += 0.1;
+            }
+        }
+
+        private int fact(int n)
+        {
+            var prod = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                prod *= i;
+            }
+            return prod;
+        }
+
+        [Test]
+        public void HCoeffsTest()
+        {
+            var solver = new OdeSolver(t => Pow(t, 3), new []{1d}, new []{1d});
+            var h = Range(0,5).Select(solver.HCoeffs).ToArray();
+        }
+
+
+        private class OdeSolverStub : OdeSolver
+        {
+            public OdeSolverStub(Func<double, double> right, double[] initialConditions, double[] coeffs) : base(right, initialConditions, coeffs)
+            {
+            }
+
+            public static int BinomsProductSum(int s, int k)
+            {
+                return OdeSolver.BinomsProductSum(s, k);
+            }
+
+            public double PSecondPart(int s)
+            {
+                return base.PSecondPart(s);
+            }
+        }
+    }
+}
